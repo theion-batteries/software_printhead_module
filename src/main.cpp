365c39370b0574d912ebdbb3df094f2790afa241
sami-theion <sami@theion.de>
2023-01-12 13:32:17 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,40 @@
 #include <chrono>
 #include <thread>
 
-meteorAdapter phInterface;
+
+void testApi1(meteorAdapter* phIntptr);
+void testApi2(meteorAdapter* phIntptr);
 
 int main()
 {
-    std::cout << "***************************  testing meteor sdk apis *****************************" << std::endl;
+   
+    testApi1(new meteorAdapter);
+}
+
+void testApi1(meteorAdapter* phIntptr)
+{
+     std::cout << "***************************  testing meteor sdk apis *****************************" << std::endl;
     std::cout << "eret functions tests" << std::endl;
     std::cout << "test succes return code: 0 " << std::endl;
     std::cout << "test error return code: see docs " << std::endl;
 
     std::cout << "test1: open connection to print engine (print engine must be running !)" << std::endl;
-    phInterface.connect();
+    phIntptr->connect();
     std::cout << "test2: set head power on" << std::endl;
  
-    phInterface.turnOnPh();
+    phIntptr->turnOnPh();
 
     std::this_thread::sleep_for(std::chrono::seconds(20));
 
     std::cout << "test3: set head power off" << std::endl;
-    phInterface.turnOffPh();
+    phIntptr->turnOffPh();
 
     std::cout << "test4: close connection to print engine (print engine must be running !)" << std::endl;
-    phInterface.disconnect();
+    phIntptr->disconnect();
 
     std::cout << "bool functions tests" << std::endl;
     std::cout << "test4: is printer busy" << std::endl;
     bool status = PiIsBusy();
     std::cout << "return code: " << status << std::endl;
-
+    delete phIntptr;
 }
-
